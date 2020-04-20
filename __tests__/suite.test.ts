94a6c250458c4a5b786c45bbccccb1c130cf6fb1
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2020-04-20 16:52:56 +0300

    feat: add jest configuration & tests haven't written the suite yet

---
+++ __tests__/suite.test.ts
@@ -0,0 +1,4 @@
+/* global test, expect */
+test('should just succeed', () => {
+	expect(true).toBeTruthy();
+});
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
-// eslint-disable
 module.exports = {
+	coverageDirectory: './coverage',
+	coverageProvider: 'v8',
+	displayName: 'Unit tests',
 	preset: 'ts-jest',
 	testEnvironment: 'node',
+	globals: {
+		'ts-jest': {
+			tsConfig: '__tests__/tsconfig.json'
+		}
+	}
 };
